09875612dcfafadd4e616544c334c4534f68dc70
Tobias Hallgren <tobbiashallgren@gmail.com>
2015-01-23 11:43:58 +0100

    asd

--- Labb1THs/Labb1THs/Program.cs
+++ Labb1THs/Labb1THs/Program.cs
@@ -6,7 +6,9 @@ namespace Laboration1TH
     {
         private const int femhundra = 500;
         private const int etthundra = 100;
+        private const int femtio = 50;
         private const int tjugo = 20;
+        private const int tio = 10;
         private const int fem = 5;
         private const int en = 1;
 
@@ -37,10 +39,10 @@ namespace Laboration1TH
                     {
                         Console.BackgroundColor = ConsoleColor.Red;
                         Console.ForegroundColor = ConsoleColor.White;
-                        Console.WriteLine("Totalsumman är för liten. Köpet kunde itne genomföras!");
+                        Console.WriteLine("Totalsumman är för liten. Köpet kunde inte genomföras!");
                         Console.ResetColor();
-
                         return;
+
                     }
                     break;
                 }
@@ -79,75 +81,89 @@ namespace Laboration1TH
                 Console.ForegroundColor = ConsoleColor.White;
                 Console.WriteLine("FEL! Erhållet belopp är för litet. Köpet kunde inte genomföras. ");
                 Console.ResetColor();
+                return;
             }
-            else
+
+            //Öresavrunding 
+            attBetala = (int)Math.Round(totalKostnad);
+            oresavrundning = attBetala - totalKostnad;
+
+            tillbaka = erhalletBelopp - attBetala;
+
+            string totalt = "Totalt";
+            string avrunding = "Öresavrundning";
+            string betala = "Att betala";
+            string erhallet = "Kontant";
+            string retur = "Tillbaka";
+
+            Console.WriteLine("\nKVITTO\n-------------------------------");
+
+            Console.WriteLine("{0,-17}:{1,12:c}\n{2,-17}:{3,12:c}\n{4,-17}:{5,12:c}\n{6,-17}:{7,12:c}\n{8,-17}:{9,12:c}",
+                totalt,
+                totalKostnad,
+                avrunding,
+                oresavrundning,
+                betala,
+                attBetala,
+                erhallet,
+                erhalletBelopp,
+                retur,
+                tillbaka);
+            Console.WriteLine("-------------------------------");
+
+            int vaxel = 0;
+            int femhundralapp = 0;
+            int femtiolapp = 0;
+            int etthundralapp = 0;
+            int tjugolapp = 0;
+            int tiokrona = 0;
+            int femkrona = 0;
+            int enkrona = 0;
+
+            vaxel = tillbaka;
+            femhundralapp = vaxel / femhundra;
+            vaxel = vaxel % femhundra;
+            etthundralapp = vaxel / etthundra;
+            vaxel = vaxel % etthundra;
+            femtiolapp = vaxel / femtio;
+            vaxel = vaxel % femtio;
+            tjugolapp = vaxel / tjugo;
+            vaxel = vaxel % tjugo;
+            tiokrona = vaxel / tio;
+            femkrona = vaxel / fem;
+            vaxel = vaxel % fem;
+            enkrona = vaxel / en;
+
+
+            if (femhundralapp > 0)
             {
-                //Öresavrunding 
-                attBetala = (int)Math.Round(totalKostnad);
-                oresavrundning = attBetala - totalKostnad;
-
-                tillbaka = erhalletBelopp - attBetala;
-
-                string totalt = "Totalt";
-                string avrunding = "Öresavrundning";
-                string betala = "Att betala";
-                string erhallet = "Kontant";
-                string retur = "Tillbaka";
-
-                Console.WriteLine("\nKVITTO\n-------------------------------");
-
-                Console.WriteLine("{0,-17}:{1,12:c}\n{2,-17}:{3,12:c}\n{4,-17}:{5,12:c}\n{6,-17}:{7,12:c}\n{8,-17}:{9,12:c}",
-                    totalt,
-                    totalKostnad,
-                    avrunding,
-                    oresavrundning,
-                    betala,
-                    attBetala,
-                    erhallet,
-                    erhalletBelopp,
-                    retur,
-                    tillbaka);
-                Console.WriteLine("-------------------------------");
-
-
-
-                int vaxel = 0;
-                int femhundralapp = 0;
-                int etthundralapp = 0;
-                int tjugolapp = 0;
-                int femkrona = 0;
-                int enkrona = 0;
-
-                string femhundravaxel = "500-lappar";
-                string etthundravaxel = "100-lappar";
-                string tjugovaxel = "20-lappar";
-                string femkronavaxel = "5-kronor";
-                string enkronavaxel = "1-kronor";
-
-                vaxel = tillbaka;
-                femhundralapp = vaxel / femhundra;
-                vaxel = vaxel % femhundra;
-                etthundralapp = vaxel / etthundra;
-                vaxel = vaxel % etthundra;
-                tjugolapp = vaxel / tjugo;
-                vaxel = vaxel % tjugo;
-                femkrona = vaxel / fem;
-                vaxel = vaxel % fem;
-                enkrona = vaxel / en;
-
-                //Skriver ut antalet sedlar/mynt som kund skall få tillbaka samt rundar ner.
-                Console.WriteLine(" {0,-16}: {1}\n {2,-16}: {3}\n {4,-16}: {5}\n {6,-16}: {7}\n {8,-16}: {9}",
-                    femhundravaxel,
-                    femhundralapp,
-                    etthundravaxel,
-                    etthundralapp,
-                    tjugovaxel,
-                    tjugolapp,
-                    femkronavaxel,
-                    femkrona,
-                    enkronavaxel,
-                    enkrona);
+                Console.WriteLine(" 500-lappar: {0}", femhundralapp);
             }
+            if (etthundralapp > 0)
+            {
+                Console.WriteLine(" 100-lappar: {0}", etthundralapp);
+            }
+            if (femtiolapp > 0)
+            {
+                Console.WriteLine(" 50-lappar: {0,3}", femtiolapp);
+            }
+            if (tjugolapp > 0)
+            {
+                Console.WriteLine(" 20-lappar: {0,2}", tjugolapp);
+            }
+            if (tiokrona > 0)
+            {
+                Console.WriteLine(" 10-kronor: {0,3}", tiokrona);
+            }
+            if (femkrona > 0)
+            {
+                Console.WriteLine(" 5-kronor: {0,3}", femkrona);
+            }
+            if (enkrona > 0)
+            {
+                Console.WriteLine(" 1-kronor: {0,3}", enkrona);
+            }
+
         }
     }
 }
---
+++ Labb3/Labb3/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Linq;
+using System.Collections;
+
+namespace labb3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            do
+            {
+                int antalLoner = 0;
+                while(true)
+                {
+                    antalLoner = ReadInt("Ange antal löner att mata in:");
+
+                    if(antalLoner >= 2)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine();
+                        Console.BackgroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Du måste mata in minst två löner för att kunna göra en beräkning! ");
+                        Console.ResetColor();
+                    }
+                }
+                
+                // nu vet du att det är minst 2 löner
+                ProcessSalaries(antalLoner);
+
+                // avsluta eller inte?
+                Console.WriteLine();
+                Console.BackgroundColor = ConsoleColor.DarkGreen;
+                Console.WriteLine("Tryck valfri tangent för en ny beräkning - ESC avslutar.");
+                Console.ResetColor();
+                
+
+            } while (Console.ReadKey().Key != ConsoleKey.Escape);
+            Console.Clear();       
+        }
+
+        static void ProcessSalaries(int count)
+        {
+
+            int loneberakning = 0;
+            int[] lonerad = new int[count];
+
+            for (int i = 0; i < lonerad.Length; i++)
+            {
+                Console.Write("Ange lön nummer {0}: ", i + 1);
+                string loner = Console.ReadLine();
+                loneberakning = int.Parse(loner);
+                lonerad[i] = loneberakning;
+            }
+            
+
+            double average = lonerad.Average();
+            int min = lonerad.Min();
+            int max = lonerad.Max();
+            int lonespridning = max - min;
+           
+            Array.Sort(lonerad);
+
+
+            Console.WriteLine();
+
+            Console.WriteLine();
+            Console.WriteLine("----------------------");
+            Console.WriteLine("Lönespridning : {0}", lonespridning);
+            Console.WriteLine("Medellön : {0}", average);
+
+            if (count % 2 == 0)
+            {
+                int medianNr1 = lonerad.Length / 2 - 1;
+                int medianNr2 = lonerad.Length / 2;
+
+                double median = (lonerad[medianNr1] + lonerad[medianNr2]) / 2.0d;
+                Console.WriteLine("Medianlön : {0}", median);
+            }
+            else
+            {
+                int median = lonerad.Length / 2;
+                Console.WriteLine("Medianlön: {0}", lonerad[median]);
+            }
+
+           
+            Console.WriteLine("----------------------");
+
+
+        
+
+        }
+
+        static int ReadInt(string prompt)
+        {
+            string angeAntalLoner;
+
+            Console.Write(prompt);
+            angeAntalLoner = Console.ReadLine();
+
+            Console.WriteLine();
+
+            return int.Parse(angeAntalLoner);
+        }
+    }
+}
+
