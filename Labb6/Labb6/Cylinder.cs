6786a6a629c543dab1c9cfe7016d83898f94a96c
Tobias Hallgren <tobbiashallgren@gmail.com>
2015-02-10 13:13:24 +0100

    lb6uplod

---
+++ Labb6/Labb6/Cylinder.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labb6
+{
+//    class Cylinder : Solid
+//    {
+//        public double BaseArea { get; }
+//        public double SurfaceArea { get; }
+//        public double Volume { get; }
+//    }
+}
