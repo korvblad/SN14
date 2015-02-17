aca0302529db30ec549760c295c10c449092f43f
Tobias Hallgren <tobbiashallgren@gmail.com>
2015-02-17 13:32:15 +0100

    omg funka ffs

--- Laboration4.A/Laboration4.A/SecretNumber.cs
+++ Laboration4.A/Laboration4.A/SecretNumber.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,41 +6,60 @@ using System.Threading.Tasks;
 
 namespace Laboration4.A
 {
-   public class SecretNumber
+    public class SecretNumber
     {
+        public const int MaxNumberOfGuesses = 7;
 
-       
-       private int _number = 0;
-       private int _count = 0;
-       public const int MaxNumberOfGuesses = 7;
-       
-       
+        private int _count;
+        private int _number;
+
         public void Initialize()
         {
-
+            Random random = new Random();
+            _number = random.Next(1, 100);
             _count = 0;
-
-            return;
-              
         }
 
         public bool MakeGuess(int number)
         {
-            throw new NotImplementedException();
-            //for (int i = 0; i < 7; i++)
-            //{
-            //    Console.Write("Gissning {0} : ", i + 1);
-            //    number = int.Parse(Console.ReadLine());
-            //}
 
+            if (_count >= MaxNumberOfGuesses)
+            {
+                throw new ApplicationException();
+            }
+
+            if (number >= 101 || number <= 0)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+
+            _count++;
+            int guessesLeft = MaxNumberOfGuesses - _count;
+
+            if (number < _number)
+            {
+                Console.WriteLine("{0} är för lågt! Du har {1} gissningar kvar!", number, guessesLeft);
+                return false;
+            }
+
+            if (number > _number)
+            {
+                Console.WriteLine("{0} är för högt. Du har {1} gissningar kvar.", number, guessesLeft);
+                return false;
+            }
+
+            if (_count == MaxNumberOfGuesses)
+            {
+                Console.WriteLine("Det hemliga talet är {0}.", _number);
+            }
+            Console.WriteLine("Bra Jobbat! Du klarade det på {0} försök.", _count);
+            Console.WriteLine("Det hemliga talet var {0}.", _number);
+            return true;
         }
-
-
 
         public SecretNumber()
         {
-            
+            Initialize();
         }
-        
     }
 }
